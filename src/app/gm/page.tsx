aac2a6c5d640a6cd942365b4bbb38ebf7c4d9f68
phanphuc2804 <phanphuc28042000@gmail.com>
2023-05-05 01:10:35 +0700

    Setup .Prettierrc

---
+++ src/app/gm/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface GMProps {
+}
+
+export default function GM (props: GMProps) {
+  return (
+    <div>
+      GM Page
+    </div>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface LoginProps {
+}
+
+export default function Login (props:  LoginProps) {
+  return (
+    <div>
+      Login Page
+    </div>
+  );
+}
